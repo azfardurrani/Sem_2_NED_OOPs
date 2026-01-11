6f786059c375f6b0bc6d72b26ef5919aff5eba0c
azfardurrani <azfarahsan302@gmail.com>
2026-01-11 13:31:18 +0000

    update

--- LABS/week_01/lab.cpp
+++ LABS/week_01/lab.cpp
@@ -2,8 +2,31 @@
 using namespace std;
 int main()
 {
+
+    /// taking input 
+
     int age;
     cout << "age: ";
     cin >> age;
     cout << "your age is " << age;
+
+    ///iterating through for loop 
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    ///factorial via for loop 
+    int fact = 1;
+    int no;
+    cout << "enter a number for factorial"<<"\n";
+    cin >>no;
+    for (int i = 1; i <= no; i ++)
+    {
+        fact = fact * i;
+    }
+    cout << "the factorial is: " << fact;
+
+
 }
---
+++ LABS/week_01/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+    // int fact = 1;
+    // int no;
+    // cout << "enter a number for factorial"<<"\n";
+    // cin >>no;
+    // for (int i = 1; i <= no; i ++)
+    // {
+    //     fact = fact * i;
+    // }
+    // cout << "the factorial is: " << fact;
