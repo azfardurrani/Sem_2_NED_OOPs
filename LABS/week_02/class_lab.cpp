6d0d76baf3fa06498058b30c3da00c0da73cb6ae
azfardurrani <azfarahsan302@gmail.com>
2026-01-15 14:11:04 +0000

    update

--- LABS/week_02/class_lab.cpp
+++ LABS/week_02/class_lab.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "enter length"<< endl;
+    cin >> a;
+    cout << "enter width"<< endl;
+    cin >> b;
+    cout << "the ara of rectangle is: "<< a*b << endl;
+
+    cout << " enter any no ";
+    cin >> c;
+    if(c % 2 == 0)
+    {
+        cout << "even"<<endl;
+    }
+    else
+    {
+        cout << " odd" << endl;
+    }
+
+    int m, f = 0, sec = 1 ,next;
+    cout<<" enter any no. : ";
+    cin >> m;
+    for (int i =1; i < m; ++i)
+    {
+        cout << f << " ";
+        next = f + sec;
+        f = sec;
+        sec = next;
+    }
+    cout << endl;
+
+}
 
