e7551d386688992b23892b5e0605eadc25d00ca0
azfardurrani <azfarahsan302@gmail.com>
2026-01-27 10:36:56 +0000

    update

--- LABS/week_03/task_lab.cpp
+++ LABS/week_03/task_lab.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// Q5) Write a C++ program to generate the multiplication table of a given number.
+    int n;
+    cout << "enter any no. : ";
+    cin >> n;
+    for(int i = 0; i <= 12; i++)
+    {
+        cout << n << " x " << i << " = " << i*n << endl;
+    }
+// Q6) Write a C++ program to create a simple menu-driven calculator that performs basic arithmetic operations (addition, subtraction, multiplication, division).
+// Q7) Write a C++ program to generate the Fibonacci series up to a given number of terms.
+// Q8) Write a C++ program to implement a number guessing game where the user tries to guess a randomly generated number.
+// Q9) Write a C++ program to implement a simple rock, paper, scissors game between the user and the computer.
+// Q10) Write a C++ program to display the name of the day of the week based on the day number entered by the user.
+// Q11) Write a C++ program to print the following patterns and shapes:
 
+}
